55317fa0c352922f1fe3335de31c8b52dc73348a
root <kabstas@rambler.ru>
2020-10-31 12:05:02 +0000

    add new features

--- courseProject/Sources/courseProject/main.swift
+++ courseProject/Sources/courseProject/main.swift
@@ -105,29 +105,14 @@ func encodingToJsonWithWritingToFile (dictionaries: [String: [String: String]]) 
     //print(path)
 }
 
-enum Action {
+enum Action: String, ExpressibleByArgument {
 
-    case search
-    case update
-    case delete
-    case anyOtherCase
-    
-    init(_ string: String) throws {
-        if string == "search" {
-            self = .search
-        } else if string == "update" {
-            self = .update
-        } else if string == "delete" {
-            self = .delete
-        } else { 
-            self = .anyOtherCase
-        }
-    }
+    case search, update, delete
 }
 
 struct Run: ParsableCommand {
     
-    @Argument(help: "search, update or delete", transform: Action.init)
+    @Argument(help: "search, update or delete")
     var action: Action
     @Argument(help: "chosen word to update") 
     var word: String?
@@ -170,8 +155,6 @@ struct Run: ParsableCommand {
                 dictionaries = deletingValuesFromDictionary (language: finalLanguage)
                 encodingToJsonWithWritingToFile (dictionaries: dictionaries)
             }          
-        default:
-            return
         }
     }
 }
